bea0a9b7cef20673b1760ca831fce907bbe0f694
XJINE <umeda@team-lab.com>
2022-06-23 19:25:20 +0900

    Add resources to make a git-package.

--- Assets/Packages/FrameRateSettings/FrameRateSettings.cs
+++ Assets/Packages/FrameRateSettings/FrameRateSettings.cs
@@ -7,39 +7,52 @@ public class FrameRateSettings : SingletonMonoBehaviour<FrameRateSettings>
     // vSyncCount = 1 means every V blank.
     // When set 2 in 60Hz, it becomes 60/2 = 30 frame/sec.
 
-    [Range(0, 4)]
-    public int vSyncCount = 1;
-
     // NOTE:
     // targetFrameRate = -1(default) is special value to fit every platform.
     // For example, when in mobile platform, frame rate will be set 30fps.
 
-    [Range(-1, 9999)]
-    public int targetFrameRate = -1;
+    public bool warningInEditor = false;
+
+    [SerializeField, Range( 0,    4)] private int vSyncCount = 1;
+    [SerializeField, Range(-1, 9999)] private int frameRate  = -1;
+
+    public int VSyncCount
+    {
+        get => vSyncCount;
+        set { vSyncCount = value; UpdateSettings(); }
+    }
+
+    public int FrameRate
+    {
+        get => frameRate;
+        set { frameRate = value; UpdateSettings(); }
+    }
 
     protected override void Awake()
     {
         base.Awake();
-
-        if (CommandLineArgs.GetValueAsInt("-vSyncCount", out int vSyncCount))
-        {
-            this.vSyncCount = vSyncCount;
-        }
-
-        if (CommandLineArgs.GetValueAsInt("-targetFrameRate", out int targetFrameRate))
-        {
-            this.targetFrameRate = targetFrameRate;
-        }
-
         UpdateSettings();
     }
 
-    public virtual void UpdateSettings()
+    private void OnValidate()
     {
-        QualitySettings.vSyncCount = this.vSyncCount;
-        Application.targetFrameRate = this.targetFrameRate;
+        UpdateSettings();
+    }
 
-        if (Application.isEditor)
+    private void ValidateParameters()
+    {
+        vSyncCount = Mathf.Max( 0, Mathf.Min(   4, vSyncCount));
+        frameRate  = Mathf.Max(-1, Mathf.Min(9999, frameRate));
+    }
+
+    private void UpdateSettings()
+    {
+        ValidateParameters();
+
+        QualitySettings.vSyncCount  = vSyncCount;
+        Application.targetFrameRate = frameRate;
+
+        if (warningInEditor && Application.isEditor)
         {
             Debug.Log("Application.targetFrameRate setting is ignored in Editor.", this);
         }
